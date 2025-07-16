a36f2c8a52fb7e9853a73d9af8220e020896f61e
Agnay Shrivastava <shrivastava.agnay283@gmail.com>
2025-07-16 09:15:36 +0530

    added routing and basic project structure

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from '../pages/Header/Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <Header/>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/7d2359a4-434f-4efa-9ff3-e9d38a8bde7f/web/IN-en-20250707-TRIFECTA-perspective_4faa9280-a2c5-4e07-aafc-a45ce43fea09_large.jpg" alt="Netflix banner image" />
+    </div>
   )
 }
 
